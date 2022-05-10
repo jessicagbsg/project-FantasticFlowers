92dc8e8d006320360b0a393f0ff7e2dc62a32668
Jessica Gondim <jessicagbsg@gmail.com>
2022-05-10 16:31:08 -0300

    accordion refactored in class

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,20 +1,27 @@
-export default function initAccordion() {
-  const activeClass = "active";
-  const accordionList = document.querySelectorAll(
-    '[data-anime="accordion"] dt'
-  );
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "active";
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
 
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+  // add events to the accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", this.toggleAccordion(item));
     });
+  }
+
+  // function starts
+  init() {
+    if (this.accordionList.length) {
+      // activate the fist item
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }
