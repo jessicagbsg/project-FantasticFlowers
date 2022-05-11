0b5f20da82213e6cabb3d7c8aad4d157e6947aa2
Jessica Gondim <jessicagbsg@gmail.com>
2022-05-11 15:39:01 -0300

    scrollAnimation redone

--- js/modules/scrollAnimation.js
+++ js/modules/scrollAnimation.js
@@ -1,22 +1,39 @@
-export default function initScrollAnimation() {
-  const activeClass = "active";
-  const sections = document.querySelectorAll('[data-anime="scroll"]');
-  const windowHalf = window.innerHeight * 0.6;
+export default class ScrollAnimation {
+  constructor(sections) {
+    this.activeClass = "active";
+    this.sections = document.querySelectorAll(sections);
+    this.windowHalf = window.innerHeight * 0.4;
+    this.checkDistance = this.checkDistance.bind(this);
+  }
 
-  function animaScroll() {
-    sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - windowHalf < 0;
-      if (isSectionVisible) {
-        section.classList.add(activeClass);
-      } else if (section.classList.contains(activeClass)) {
-        section.classList.remove(activeClass);
+  // gets distance of each section from scroll top
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowHalf),
+      };
+    });
+  }
+
+  // checks distance of each section from scroll top
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add(this.activeClass);
+      } else if (item.element.classList.contains(this.activeClass)) {
+        item.element.classList.remove(this.activeClass);
       }
     });
   }
 
-  if (sections.length) {
-    animaScroll();
-    window.addEventListener("scroll", animaScroll);
+  init() {
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
   }
 }
